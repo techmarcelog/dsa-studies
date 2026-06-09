7b333c85a8488c25b65fdafb9bc27ceed943b514
Marcelo Sousa <vinicusm466@gmail.com>
2026-06-08 22:21:38 -0300

    feat: add Reverse Words in a String III in TwoPointer folder

---
+++ problemas/TwoPointer/ReverseWordsInAStringString.java
@@ -0,0 +1,15 @@
+package problemas.TwoPointer;
+
+public class ReverseWordsInAStringString{
+    public String reverseWords(String s)
+    {
+        String[] words = s.split(" ");
+        
+        for(int i = 0; i< words.length; i++)
+        {
+            words[i] = new StringBuilder(words[i]).reverse().toString();
+        }
+
+        return String.join(" ", words);
+    }
+}
